02a407f06aba827d60eab4e81c098a661a944786
Starry Xander <starryxander@gmail.com>
2025-10-30 04:15:45 +0100

    feat: add current year utility, social media colors, and todo links handler
    - Implement current-year.js utility to dynamically update copyright year
    - Add social media brand colors to CSS variables
    - Create todo-links.js development utility to handle placeholder links
    - Update footer social icons to use brand colors on hover
    - Add new utility initializers to main script.js
    - Update mobile navigation links with consistent TODO placeholders

--- src/js/script.js
+++ src/js/script.js
@@ -5,10 +5,12 @@
  */
 'use strict';
 
-// --- Import Initializers from Global Modules ---
+// --- Import Initializers from Global & Utility Modules ---
 import { initNavigation } from './components/navigation.js';
 import { initSvgInjector } from './components/svg-injector.js';
 import { initAnimateOnView } from './components/animate-on-view.js';
+import { initTodoLinks } from './utils/todo-links.js'; 
+import { initCurrentYear } from './utils/current-year.js'; 
 
 // --- Page-Specific Initializers ---
 import { initHomePage } from './pages/home.js';
@@ -19,6 +21,8 @@ function main() {
     initNavigation();
     initSvgInjector();
     initAnimateOnView();
+    initTodoLinks();      // 3. RUN TODO SCRIPT
+    initCurrentYear();    // 4. RUN YEAR SCRIPT
     
     // Check if we are on the homepage and run its specific logic
     if (document.body.id === 'page-home') {
