0794b75fef0c52fa7fd7bc45d3c9519aef8bbb77
Starry Xander <starryxander@gmail.com>
2026-03-08 04:43:28 +0100

    fix: Adjust Vite Netlify plugin import and add .netlify to gitignore.

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
+import { defineConfig } from 'vite';
 import { resolve } from 'path';
-import { netlifyPlugin } from '@netlify/vite-plugin';
+import netlifyPlugin from '@netlify/vite-plugin';
 
 export default defineConfig({
   root: 'src', // Your source files are here
